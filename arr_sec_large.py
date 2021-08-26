8d2f042307e7e914521ea8094dbdd6b51d3e88f2
Ravi Teja_Muvvala <79572156+ravitejamuvvala@users.noreply.github.com>
2021-08-26 10:48:36 +0530

    Add files via upload

---
+++ arr_sec_large.py
@@ -0,0 +1,25 @@
+def second_largest(list):
+    maximum= max(list[0],list[1])
+    second_max= min(list[0],list[1])
+    n=len(list)
+    for i in range(2, n):
+        if list[i]>maximum:
+            second_max=maximum
+            maximum=list[i]
+        else:
+            if list[i]>second_max:
+                second_max=list[i]
+            
+    return second_max
+
+
+#input of list
+li=[]
+n=int(input("Enter size of list "))
+e=int(input("Enter elements of list "))
+for i in range(0,n):
+    li.append(e)
+
+#smallest
+print("second largest in ",li,"is",end=" ")
+print(second_largest(li))        
