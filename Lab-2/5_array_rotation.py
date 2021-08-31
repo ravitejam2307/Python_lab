3124e0a2567438becee92fbadc127271f126de56
Ravi Teja_Muvvala <79572156+ravitejamuvvala@users.noreply.github.com>
2021-08-31 23:34:25 +0530

    Add files via upload

---
+++ Lab-2/5_array_rotation.py
@@ -0,0 +1,24 @@
+arr = [1, 2, 3, 4, 5];     
+   
+n = 3;    
+
+print("Original array: ")    
+for i in range(0, len(arr)):    
+    print(arr[i]),     
+         
+for i in range(0, n):       
+    first = arr[0];    
+        
+    for j in range(0, len(arr)-1):
+            
+        arr[j] = arr[j+1];    
+            
+        
+    arr[len(arr)-1] = first;    
+     
+print();    
+     
+    
+print("Array after left rotation: ");    
+for i in range(0, len(arr)):    
+    print(arr[i]),    
