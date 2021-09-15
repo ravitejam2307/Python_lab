0125a8d13efcde2f9e0f66b1b9e56d91c18ed959
Ravi Teja_Muvvala <79572156+ravitejam2307@users.noreply.github.com>
2021-09-15 15:05:26 +0530

    Delete Lab-2 directory

--- Lab-2/12_Counting_frequencies.py
+++
@@ -1,9 +0,0 @@
-list = ['a','b','a','c','d','c','c']
-frequency = {}
-for item in list:
-   if (item in frequency):
-      frequency[item] += 1
-   else:
-      frequency[item] = 1
-for key, value in frequency.items():
-   print("% s -> % d" % (key, value))
