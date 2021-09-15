89c838a1f095cf4f8366f4be5902242f1877b44b
Ravi Teja_Muvvala <79572156+ravitejam2307@users.noreply.github.com>
2021-09-15 15:07:01 +0530

    Create 5_array_rotation.py

---
+++ Lab Cycle/5_array_rotation.py
@@ -0,0 +1,44 @@
+#Array Rotation
+
+arr = []
+
+n = int(input("Enter number of elements : "))
+ 
+for i in range(0, n):
+    ele = int(input())
+ 
+    arr.append(ele) 
+
+n = int(input("Enter n value for Left Rotation:"))
+
+print("Given Array Elements are")
+for i in range(0, len(arr)):    
+    print(arr[i],end=" ") 
+         
+#Array Left Rotation
+for i in range(0, n):       
+    first = arr[0];    
+        
+    for j in range(0, len(arr)-1):
+            
+        arr[j] = arr[j+1];    
+            
+        
+    arr[len(arr)-1] = first;
+print("Array after left rotation: ");    
+for i in range(0, len(arr)):    
+    print(arr[i],end=" ")
+    
+#Array Right Rotation
+n = int(input("\nEnter n value for Right Rotation:"))
+for i in range(0, n):       
+    last = arr[len(arr)-1];    
+        
+    for j in range(len(arr)-1,-1,-1):
+            
+        arr[j] = arr[j-1];    
+            
+    arr[0] = last;         
+print("Array after Right rotation: ");    
+for i in range(0,len(arr)):    
+    print(arr[i],end=" ")
