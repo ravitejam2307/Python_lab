a7627bb06c3c9c46a90a9db36b75bf3a324f0388
Ravi Teja_Muvvala <79572156+ravitejam2307@users.noreply.github.com>
2021-09-18 16:38:56 +0530

    Create 15_file.py

---
+++ Lab Cycle/15_file.py
@@ -0,0 +1,24 @@
+#SAMPLE.TXT
+
+Hello World
+Hello Again
+Goodbye
+
+#file.py
+
+file = open("sample.txt", "r")
+
+number_of_lines = 0
+number_of_words = 0
+number_of_characters = 0
+for line in file:
+  line = line.strip("\n")
+  
+  words = line.split()
+  number_of_lines += 1
+  number_of_words += len(words)
+  number_of_characters += len(line)
+
+file.close()
+
+print("lines:", number_of_lines, "words:", number_of_words, "characters:", number_of_characters)
